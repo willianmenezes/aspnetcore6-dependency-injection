65fa6a5688b543fe26574c9b29e4b49b95825f28
Willian Menezes <willian_menezes_santos@hotmail.com>
2022-01-14 23:42:55 -0300

    first commit

---
+++ IOperacao.cs
@@ -0,0 +1,17 @@
+﻿namespace InjecaoDeDependencia
+{
+    public interface IOperacao
+    {
+        Guid Id { get; }
+    }
+
+    public class Operacao : IOperacao
+    {
+        public Guid Id { get; set; }
+
+        public Operacao()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
